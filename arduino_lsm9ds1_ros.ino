721a6f99da8f6829fa75404f337f7b890e89c016
Evan Krall <evan@evankrall.com>
2019-05-28 23:03:20 -0700

    initial commit

---
+++ arduino_lsm9ds1_ros.ino
@@ -0,0 +1,125 @@
+// The SFE_LSM9DS1 library requires both Wire and SPI be
+// included BEFORE including the 9DS1 library.
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+
+#include <ros.h>
+#include <std_msgs/String.h>
+
+//////////////////////////
+// LSM9DS1 Library Init //
+//////////////////////////
+// Use the LSM9DS1 class to create an object. [imu] can be
+// named anything, we'll refer to that throught the sketch.
+LSM9DS1 imu;
+
+//Set up the ros node and publisher
+std_msgs::String imu_msg;
+ros::Publisher imu_publisher("imu", &imu_msg);
+ros::NodeHandle nh;
+
+///////////////////////
+// Example I2C Setup //
+///////////////////////
+// SDO_XM and SDO_G are both pulled high, so our addresses are:
+#define LSM9DS1_M 0x1E // Would be 0x1C if SDO_M is LOW
+#define LSM9DS1_AG  0x6B // Would be 0x6A if SDO_AG is LOW
+
+////////////////////////////
+// Sketch Output Settings //
+////////////////////////////
+#define PRINT_CALCULATED
+//#define PRINT_RAW
+#define PRINT_SPEED 250 // 250 ms between prints
+static unsigned long lastPrint = 0; // Keep track of print time
+
+// Earth's magnetic field varies by location. Add or subtract 
+// a declination to get a more accurate heading. Calculate 
+// your's here:
+// http://www.ngdc.noaa.gov/geomag-web/#declination
+#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+
+void setup() 
+{
+  
+  Serial.begin(115200);
+  
+  // Before initializing the IMU, there are a few settings
+  // we may need to adjust. Use the settings struct to set
+  // the device's communication mode and addresses:
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  // The above lines will only take effect AFTER calling
+  // imu.begin(), which verifies communication with the IMU
+  // and turns it on.
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    while (1)
+      ;
+  }
+
+  nh.initNode();
+  nh.advertise(imu_publisher);
+}
+
+void loop()
+{
+  // Update the sensor values whenever new data is available
+  if ( imu.gyroAvailable() )
+  {
+    // To read from the gyroscope,  first call the
+    // readGyro() function. When it exits, it'll update the
+    // gx, gy, and gz variables with the most current data.
+    imu.readGyro();
+  }
+  if ( imu.accelAvailable() )
+  {
+    // To read from the accelerometer, first call the
+    // readAccel() function. When it exits, it'll update the
+    // ax, ay, and az variables with the most current data.
+    imu.readAccel();
+  }
+  if ( imu.magAvailable() )
+  {
+    // To read from the magnetometer, first call the
+    // readMag() function. When it exits, it'll update the
+    // mx, my, and mz variables with the most current data.
+    imu.readMag();
+  }
+  
+  String data =
+    String(imu.gx) + " " +
+    String(imu.gy) + " " +
+    String(imu.gz) + " " +
+    String(imu.ax) + " " +
+    String(imu.ay) + " " +
+    String(imu.az) + " " +
+    String(imu.mx) + " " +
+    String(imu.my) + " " +
+    String(imu.mz) + "\n";
+
+//  Serial.write(data);
+
+  int length = data.indexOf("\n") + 2;
+  char data_final[length+1];
+  data.toCharArray(data_final, length+1);
+
+  if ((lastPrint + PRINT_SPEED) < millis())
+  {
+
+    // step 1: request reading from sensor
+    imu_msg.data = data_final;
+    imu_publisher.publish(&imu_msg);
+    nh.spinOnce();
+
+    lastPrint = millis(); // Update lastPrint time
+  }
+}
